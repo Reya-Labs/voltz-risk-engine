d9873166cf781706d620d296f192a6e66f03a5cb
Mike Nelson <mikenelson@Mikes-MacBook-Pro-3.local>
2022-06-06 17:11:45 +0100

    Fixes to TestCalibrator

--- TestCalibrator.py
+++ TestCalibrator.py
@@ -5,7 +5,7 @@ from Calibrator import Calibrator
 
 
 # Test using example scraped APY data from Aave
-TOP_DIR = "./test_data/"
+TOP_DIR = "./historical_data/"
 DF_PROTOCOL = pd.read_csv(TOP_DIR+"composite_df_AaveVariable_apy.csv")
 DF_PROTOCOL.dropna(inplace=True)
 DF_PROTOCOL.set_index("Date", inplace=True)
@@ -13,7 +13,7 @@ DISC_DRIFT = 0.99
 
 class TestCalibrator(unittest.TestCase):
     
-    def instantiate(self):
+    def setUp(self):
         self.calibrator = Calibrator(df_protocol=DF_PROTOCOL, disc_drift=DISC_DRIFT)
     
     def test_calibrator(self):
@@ -48,7 +48,7 @@ class TestCalibrator(unittest.TestCase):
                         "disc_drift_optimum": 0.9444963455200194
                     }
         }
-        for key, value in expected.items()
+        for key, value in expected.items():
             for key_, value_ in expected[key].items():
                 self.assertAlmostEqual(RESIDUAL_MIN[key][key_], value_) # Almost equal due to differences when running the minimiser
 
